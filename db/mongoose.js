c84bc2d21f1943fe71198ba6c9d5aea501ec800f
mithleshfreak <best1466@gmail.com>
2018-01-31 15:44:51 +0545

    Before Deploying

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/BlogApp');
+// mongoose.connect('mongodb://localhost:27017/BlogApp');
 
-
+mongoose.connect('mongodb://mithlesh:rockit1466@ds121088.mlab.com:21088/blogapp');
 module.exports = {
   mongoose: mongoose
 };
